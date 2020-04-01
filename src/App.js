b177aa0866a4d024774170ddda5b36682b01cbd1
RamiroVega <ramiro.vega1993@gmail.com>
2020-03-31 19:48:05 -0500

    commit inicial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/styles.scss'
+import Curso from './Curso';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+  <>
+    <div className="main-banner img-container l-section" id="main-banner">
+      <div className="ed-grid lg-grid-6">
+        <div className="lg-cols-4 lg-x-2">
+          <img className="main-banner__img" alt="Reac.js" src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQLQtsivLTUdfdebOBaGX9ehnMc0cv82vtPBxmPpezZxtopQh1W&usqp=CAU"/>
+          <div className="main-banner__data s-center">
+            <p className="t2 s-mb-0">Curso de react.js</p>
+            <p> Edteam</p>
+            <a  href="https://ed.team" className="button">Suscribirse</a>
+          </div>
+        </div>
+      </div>
+  </div>
+
+    <div className="ed-grid m-grid-3">
+      <Curso/>
+      <Curso/>
+      <Curso/>
+      <Curso/>
+      <Curso/>
+      <Curso/>
+      <Curso/>
+      <Curso/>
+      <Curso/>
+      <Curso/>
+      <Curso/>
+      <Curso/>
+      <Curso/>
+      <Curso/>
+      <Curso/>
     </div>
-  );
-}
+  </>
+);
 
 export default App;
